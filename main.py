d2b772cc5accd04c525a6ffe53ab8392502e6a25
Jordan Kruse <jrkruse26@gmail.com>
2020-08-13 23:34:01 -0400

    finalize tkinter dev

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import configparser
 import discord.ext.commands
 import threading
+import scroller
 
 
 class MainApp(tk.Tk):
@@ -10,7 +11,7 @@ class MainApp(tk.Tk):
         self.client = None
         self.start_discord()
 
-        master = tk.Canvas(self, height=1000, width=1200)
+        master = tk.Canvas(self, height=1000, name='!master', borderwidth=10)
         master.pack()
         master.pack_propagate(0)
 
@@ -25,29 +26,37 @@ class MainApp(tk.Tk):
         keyfile = configparser.ConfigParser()
         keyfile.read('keys.config')
 
-        x = threading.Thread(target=self.client.run, args=(keyfile['Discord']['jummybot'],), daemon=True)
+        x = threading.Thread(target=self.client.run, args=(keyfile['Discord']['jummybot'],), kwargs={'bot': True}, daemon=True)
         x.start()
         return x
 
     class ServerList(tk.Frame):
         def __init__(self, master, client, propviewer):
             tk.Frame.__init__(self, master)
+            self.server_scroller = None
             self.client = client
             self.update_list()
             self.propviewer = propviewer
 
         def update_list(self):
+            if self.server_scroller is None:
+                self.server_scroller = scroller.Scroller(self)
+                self.server_scroller.canvas.configure(width=200)
+                self.server_scroller.pack()
             server_list = [x.id for x in self.client.guilds]
-            button_list = [v.id for k, v in self.children.items()]
+            button_list = [v.id for k, v in self.server_scroller.frame.children.items()]
 
             for item in [x for x in server_list if x not in button_list]:
-                button = self.Button(self, self.client.get_guild(item), self.propviewer)
-                button['text'] = self.client.get_guild(item)
-                button['command'] = button.on_press
-                button.pack()
+                if self.client.get_guild(item) is not None:
+                    button = self.Button(self.server_scroller.frame, self.client.get_guild(item), self.propviewer)
+                    button['text'] = self.client.get_guild(item).name
+                    button['command'] = button.on_press
+                    button['anchor'] = 'w'
+                    button.pack(fill=tk.X)
 
-            remove = [v for k, v in self.children.items() if v.id not in server_list]
+            remove = [v for k, v in self.server_scroller.frame.children.items() if v.id not in server_list]
             for button in remove:
+                pass
                 button.destroy()
 
             self.after(1000, self.update_list)
@@ -66,71 +75,60 @@ class MainApp(tk.Tk):
         def __init__(self, parent):
             tk.Frame.__init__(self, parent)
             self.innerframe = None
+            self.obj = None
 
-        class InnerFrame(tk.Frame):
-            def __init__(self, parent, obj):
-                tk.Frame.__init__(self, parent)
-                self.obj = obj
-
-                self.canvas = tk.Canvas(self, borderwidth=0, height=400, width=300)
-                self.frame = tk.Frame(self.canvas)
-                self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-                self.canvas.configure(yscrollcommand=self.vsb.set)
-
-                self.vsb.pack(side="right", fill="y")
-                self.canvas.pack(side="left", fill="both", expand=True)
-                self.canvas.create_window((4, 4), window=self.frame, anchor="nw",
-                                          tags="self.frame")
-
-                self.frame.bind("<Configure>", self.onFrameConfigure)
-
+        def update_viewer(self, obj):
+            self.obj = obj
+            if self.innerframe is not None:
+                self.innerframe.destroy()
+                self.innerframe = scroller.Scroller(self)
+                self.innerframe.pack()
                 self.populate()
 
-                self.canvas.bind('<Enter>', self._bound_to_mousewheel)
-                self.canvas.bind('<Leave>', self._unbound_to_mousewheel)
+            else:
+                self.innerframe = scroller.Scroller(self)
+                self.innerframe.pack()
+                self.populate()
 
-                return
+        def populate(self):
 
-            def _bound_to_mousewheel(self, event):
-                self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+            obj_type = type(self.obj).__name__
 
-            def _unbound_to_mousewheel(self, event):
-                self.canvas.unbind_all("<MouseWheel>")
-
-            def _on_mousewheel(self, event):
-                self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
-
-            def onFrameConfigure(self, event):
-                """Reset the scroll region to encompass the inner frame"""
-                self.canvas.configure(scrollregion=self.canvas.bbox("all"))
-
-            def populate(self):
+            attrs = []
+            if obj_type == 'list':
+                for item in self.obj:
+                    attrs.append(['', item, obj_type])
+            elif obj_type == 'dict':
+                for k, v in self.obj.items():
+                    attrs.append([k, v, obj_type])
+            else:
                 x = dir(self.obj)
-                attrs = []
                 for prop in x:
                     attr = getattr(self.obj, prop)
                     attr_type = type(attr).__name__
                     if attr_type not in ['method-wrapper', 'method', 'builtin_function_or_method'] and prop[:2] != '__':
                         attrs.append([prop, attr, attr_type])
 
-                for attr in attrs:
-                    if attr[2] in ['str', 'int', 'NoneType']:
-                        disp = tk.Label(text=f'{attr[0]}: {attr[1]}', master=self.frame)
-                    else:
-                        disp = tk.Button(text=f'{attr[0]}', master=self.frame)
-                    disp.grid()
+            for attr in attrs:
+                if attr[2] in ['str', 'int', 'NoneType']:
+                    disp = tk.Label(text=f'{attr[0]}: {attr[1]}', master=self.innerframe.frame, anchor='w')
+                else:
+                    disp = self.Button(self.innerframe.frame, attr[1], self)
+                    disp['text'] = f'{attr[0]}'
+                    disp['anchor'] = 'w'
+                disp.pack(fill=tk.X)
 
-        def update_viewer(self, obj):
-            if self.innerframe is not None:
-                self.innerframe.destroy()
-                self.innerframe = self.InnerFrame(self, obj)
-                self.innerframe.pack()
+        class Button(tk.Button):
+            def __init__(self, master, obj, propviewer):
+                tk.Button.__init__(self, master)
+                self.obj = obj
+                self.propviewer = propviewer
+                self['command'] = self.on_press
 
-            else:
-                self.innerframe = self.InnerFrame(self, obj)
-                self.innerframe.pack()
+            def on_press(self):
+                self.propviewer.update_viewer(self.obj)
 
 
 app = MainApp()
-app.geometry('800x400')
+app.geometry('800x500')
 app.mainloop()
---
+++ scroller.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+
+class Scroller(tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.id = None
+
+        self.canvas = tk.Canvas(self, height=400, borderwidth=2, relief="solid")
+        self.frame = tk.Frame(self.canvas, borderwidth=0, relief="solid")
+        self.vsb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.canvas.create_window((4, 4), window=self.frame, anchor="nw",
+                                  tags="self.frame")
+
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+
+        self.canvas.bind('<Enter>', self._bound_to_mousewheel)
+        self.canvas.bind('<Leave>', self._unbound_to_mousewheel)
+
+        return
+
+    def _bound_to_mousewheel(self, event):
+        self.canvas.bind_all("<MouseWheel>", self._on_mousewheel)
+
+    def _unbound_to_mousewheel(self, event):
+        self.canvas.unbind_all("<MouseWheel>")
+
+    def _on_mousewheel(self, event):
+        self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+
+    def onFrameConfigure(self, event):
+        """Reset the scroll region to encompass the inner frame"""
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
